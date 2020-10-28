87931f3c32e4cab2aece5c8e966f7af009523f6e
TheMonsther <omega.renan.rodrigues@gmail.com>
2020-10-28 17:53:30 -0300

    Fist project commit declaration

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniumAutomatization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
